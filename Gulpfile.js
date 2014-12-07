7409108c6ef41b71ce948f03eb064fba1b456a37
Stef van den Ham <stef@mindthecode.com>
2014-12-07 17:56:18 +0100

    SASS

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,9 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var jshint = require('gulp-jshint');
 
 gulp.task('browserify', function() {
   var b = browserify();
@@ -10,4 +13,23 @@ gulp.task('browserify', function() {
   return b.bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./dist'));
+});
+
+// Styles task
+gulp.task('styles', function() {
+  gulp.src('./client/styles/*.scss')
+  // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
+  .pipe(sass({onError: function(e) { console.log(e); } }))
+  // Optionally add autoprefixer
+  .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
+  // These last two should look familiar now :)
+  .pipe(gulp.dest('./dist/css/'));
+});
+
+// Views task
+gulp.task('views', function() {
+  // Get our index.html
+  gulp.src('./client/index.html')
+  // And put it in the public folder
+  .pipe(gulp.dest('./dist/'));
 });
